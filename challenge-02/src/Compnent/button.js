2fbf07ef97c13047363c1d069c2d5e487ed254e4
nouryy17 <dacinour16@gmail.com>
2024-07-28 15:27:00 +0200

    Add a solution to the -02 challenge

--- challenge-02/src/Compnent/button.js
+++ challenge-02/src/Compnent/button.js
@@ -1,7 +1,7 @@
 import "./style/button.css"
 export default function Button({title,Emoji="",children=""}) {
     return(
-        <button>
+        <button style={{height:"fit-content"}} className={title =="جديدة" ? "new" :""}>
             <p>{title}</p>
             <p>{Emoji}</p>
             {children}
