4d4da490e3c52a2efc0f48729cee13b1ed1204ef
nouryy17 <dacinour16@gmail.com>
2024-07-30 20:31:02 +0200

    add soltion

--- challenge-03/src/Compnent/content.js
+++ challenge-03/src/Compnent/content.js
@@ -1,13 +1,24 @@
 import Post from "./post";
 import "./style/content.css"
 export default function Content() {
-    return(
-    <div className="content">
-    <Post description="أكادمية ترميز لتعليم البرمجة بمختلف لغاتها و تقنياتها" titel="أكادمية ترميز">
-        <h1>20</h1>
-    </Post>
-    <Post description="this is the hello world articl" titel="hello world"/>
-    <Post description="this is the bdy of post 3" titel="Post 3"/>
-    </div>
-    );
+    const ArryPOST=[{
+        description:"أكادمية ترميز لتعليم البرمجة بمختلف لغاتها و تقنياتها",
+        titel:"أكادمية ترميز",
+        c:
+            <h1>20</h1>,
+        id:1
+    },{
+        description:"this is the hello world articl",
+        titel:" hello world",
+        id:2
+    },
+    {
+        description:"this is the bdy of post 3",
+        titel:"Post 3",
+        id:3
+    }]
+    const post=ArryPOST.map((post) =>{
+        return (<Post key={post.id} titel={post.titel} description={post.description}>{post.c}</Post>)
+    })
+   
 }
--- challenge-03/src/Compnent/sidebar.js
+++ challenge-03/src/Compnent/sidebar.js
@@ -1,16 +1,23 @@
 import Button from "./button"
 import "./style/sidebar.css"
 export default function Sidebar() {
-    return(
-        <div className="sidebar" style={{height:"fit-content",flexBasis:"30%"}}>
-           <Button title="جديدة" ><span>💫💫💫</span> </Button> 
-           <Button title="الاكثر قراءة" >
-            <img  src="https://th.bing.com/th/id/OIP.IsIw032146b-QixjeF0N1gHaEO?pid=ImgDet&w=474&h=270&rs=1" style={{width:"100%"}}></img>
-           </Button>
-           <Button title="مقالات قراءة"  Emoji="⭐️⭐️⭐️">
-            <img src="https://th.bing.com/th/id/OIP.Cn4A6_zgmKm03gm3VKuv-QHaKe?rs=1&pid=ImgDetMain"  style={{width:"100%",height:"fit-content"}}></img>
-           </Button>
-
-        </div>
+    const Arrybutton=[{
+        title:"جديدة",
+        id:1,
+    },{
+        title:"الاكثر قراءة",
+        id:2,
+        c:(<img src="https://th.bing.com/th/id/OIP.IsIw032146b-QixjeF0N1gHaEO?pid=ImgDet&w=474&h=270&rs=1" style={{width:"100%"}}></img>)
+    },{
+        title:"مقالات قراءة",
+        Emoji:"⭐️⭐️⭐️",
+        id:3,
+        c:(<img  src="https://th.bing.com/th/id/OIP.Cn4A6_zgmKm03gm3VKuv-QHaKe?rs=1&pid=ImgDetMain"  style={{width:"100%",height:"fit-content"}}></img>)
+    }]
+    const butto=Arrybutton.map((button) =>{
+    return (
+        <Button title={button.Emoji} key={button.title}> {button.c} </Button> 
     )
+    })
+   
 }
