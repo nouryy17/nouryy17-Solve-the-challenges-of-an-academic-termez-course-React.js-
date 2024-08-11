b7eeb6ce86492e149968d0065fdbdeb095357a9a
nouryy17 <dacinour16@gmail.com>
2024-08-11 15:09:23 +0200

    Add a solution challange-07

--- challenge-07/src/App.js
+++ challenge-07/src/App.js
@@ -1,42 +1,39 @@
-import './App.css';
 import { useState } from 'react';
 
-function App() {
-  return(
-    export default function RequestTracker() {
-      const [pending, setPending] = useState(0);
-      const [completed, setCompleted] = useState(0);
-    
-      async function handleClick() {
-        setPending(pending + 1);
-        await delay(3000);
-        setPending(pending - 1);
-        setCompleted(completed + 1);
-      }
-    
-      return (
-        <>
-          <h3>
-            Pending: {pending}
-          </h3>
-          <h3>
-            Completed: {completed}
-          </h3>
-          <button onClick={handleClick}>
-            Buy     
-          </button>
-        </>
-      );
-    }
-    function delay(ms) {
-      return new Promise(resolve => {
-        setTimeout(resolve, ms);
-      });
-    }
-  )
+export default function RequestTracker() {
+  const [pending, setPending] = useState(0);
+  const [completed, setCompleted] = useState(0);
 
+  async function handleClick() {
+    setPending((p) =>{
+      return p+1
+    });
+    await delay(3000);
+    setPending((p)=>{
+      return p-1
+    })
+    setCompleted((c) =>{
+      return c+1
+    });
+  }
 
-
+  return (
+    <>
+      <h3>
+        Pending: {pending}
+      </h3>
+      <h3>
+        Completed: {completed}
+      </h3>
+      <button onClick={handleClick}>
+        Buy     
+      </button>
+    </>
+  );
 }
 
-export default App;
+function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
