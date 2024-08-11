681bfe9471742184934c08b0534c1f82051a0678
nouryy17 <dacinour16@gmail.com>
2024-08-11 12:17:49 +0200

    rm challenge-08

--- challenge-05/src/App.js
+++ challenge-05/src/App.js
@@ -1,47 +1,10 @@
 import './App.css';
-import { useState } from 'react';
+import S from "./S.js"
 
 function App() {
- const initialProducts = [{
-  id: 0,
-  name: 'Baklava',
-  count: 1,
-}, {
-  id: 1,
-  name: 'Cheese',
-  count: 5,
-}, {
-  id: 2,
-  name: 'Spaghetti',
-  count: 2,
-}];
- function ShoppingCart() {
-    const [
-      products,
-      setProducts
-    ] = useState(initialProducts)
-  
-    function handleIncreaseClick(productId) {
-  
-    }
-  
-    return (
-      <ul>
-        {products.map(product => (
-          <li key={product.id}>
-            {product.name}
-            {' '}
-            (<b>{product.count}</b>)
-            <button onClick={() => {
-              handleIncreaseClick(product.id);
-            }}>
-              +
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
-  }
+return(
+  <S></S>
+)
 }
-ShoppingCart()
+
 export default App;
---
+++ challenge-05/src/S.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+export default function ShoppingCart() {
+    const [
+      products,
+      setProducts
+    ] = useState({initialProducts:[{
+        id: 0,
+        name: 'Baklava',
+        count: 1,
+      }, {
+        id: 1,
+        name: 'Cheese',
+        count: 5,
+      }, {
+        id: 2,
+        name: 'Spaghetti',
+        count: 2,
+      }]})
+  
+    function handleIncreaseClick(productId) {
+       return productId = productId +1
+    }
+  
+    return (
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.name}
+            {' '}
+            (<b>{product.count}</b>)
+            <button onClick={() => {
+              handleIncreaseClick(product.count);
+            }}>
+              +
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
